efce955d6b45998126eda320a29b7d5513d7442b
sttagami2 <sttagami2@gmail.com>
2020-03-07 18:38:00 +0900

    [Start] 4-8

---
+++ config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/new'
+  get 'books/create'
+  get 'books/edit'
+  get 'books/destroy'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
