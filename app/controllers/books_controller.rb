88702852911de804aece12195ca7dabe3151c65f
sttagami2 <sttagami2@gmail.com>
2020-03-08 11:19:15 +0900

    [Update] 4-8 うる覚えの状態でとりあえず作成してみたため、合っているのか分からない　→ 再度学習し直してから作り直そうと思う

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,43 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    # @booksはインスタンス変数であり、取得したデータをViewに渡す役目
+    # Book.allはbooksテーブルから全てのデータを取得し、インスタンス変数@booksに代入する役目
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
+    # 空のインスタンスをViewに渡す
+    # 新規データ登録用のbookインスタンスを、Bookモデルから作成する
+    # Book.newと記述すると、空のモデルが生成される
+    # インスタンス変数を書いた空のモデルを渡すと、フォームとBookモデルが関連づけられる
   end
 
   def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path(book.id)
   end
 
   def edit
+    @book = Books.find(params[:id])
   end
 
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+    # フォームから送られてきた投稿を、createアクションに保存する処理
+    # Strong Parametersを使うと、フォームからのデータを受け取れるようになる。
+    # とても大切！ → 覚えておこう
   end
 end
