eaec3894fc42f3eb3fac2915cfe4cae39a09786f
sttagami2 <sttagami2@gmail.com>
2020-03-08 14:30:39 +0900

    [Update] 4-8 indexページと投稿機能は完成。edit, showページもhtmlでは作成済み。ただし、CSSは一切記述していない。また、今後はroute.rbにてルーティング先を指定していく必要がある。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,19 +1,33 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/show'
   end
 
   def show
   end
 
-  def new
-  end
-
-  def create
-  end
+  # def new
+  #   @book = Book.new
+  # end
 
   def edit
   end
 
-  def destroy
+  def delete
   end
+
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end     # ← 記述忘れがち！
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/create'
-  get 'books/edit'
-  get 'books/destroy'
+  # get 'books/index'
+  # get 'books/show'
+  # get 'books/new'
+  # get 'books/edit'
+  # get 'books/delete'
+  resources :books
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  post 'books' => 'books#create'
+
 end
+
