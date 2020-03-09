8c11eec4eeb52b184179ecaf6dac301f60cb2e18
sttagami2 <sttagami2@gmail.com>
2020-03-09 14:04:38 +0900

    [Update] 4-8 バリデーション機能を追加。途中renderの使い方が分かっておらず、エラーばかりだった。renderはコントローラを介さずにデータを返すため、renderを定義する前に返すデータを定義する必要あり。次やることは、バリデーションエラーの表示。

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,33 +1,48 @@
 class BooksController < ApplicationController
-  def index
+  
+  # ・データベースに保存されているデータを読み込み表示するページ
+  # ・ユーザーが新たに本の題名と内容を保存するページ
+  def index    
     @book = Book.new
     @books = Book.all     # dbに保存されてる一覧?
   end
 
+
+  # ・ユーザーが新たに本の題名と内容を保存する時のアクション
   def create
     book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    if book.save
+      redirect_to book_path(book.id)        # Show.htmlの画面に移行
+    else
+      @book = Book.new
+      @books = Book.all   
+      render :index                         # renderはコントローラを介さずにViewへデータを返す → 返すデータをrender前に定義すること！
+    end
   end
 
+
+  # ・データベースに保存されている各データの詳細を表示するページ
   def show
     @book = Book.find(params[:id])
   end
 
   # def new
-  #   @book = Book.new
+
   # end
 
+  # ・データベースに保存されている各データを編集するためのページ
   def edit
     @books = Book.find(params[:id])
   end
 
+  # ・編集後にデータベースに再度保存する時のアクション
   def update
     book = Book.find(params[:id])
     book.update(book_params)
     redirect_to book_path(book.id)
   end
 
+  # ・本の題名と内容を削除するためのアクション
   def destroy
     book = Book.find(params[:id])
     book.destroy
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ApplicationRecord
-  validates :title, :body, presence: true
+  validates :title, presence: true, on: :create
+  validates :body, presence: true, on: :create
+
   # 参考ページ：https://qiita.com/nemui_yo_yeah/items/99d59b4afb7d693d085c
 end
